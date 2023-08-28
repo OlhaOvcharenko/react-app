c52bebaf52cde3a8e1a29ce94492b312a8606290
Olha Ovcharenko <ovcharenko.olga.2014@gmail.com>
2023-08-28 21:56:59 +0300

    add files with styles

---
+++ src/components/List/List.js
@@ -0,0 +1,23 @@
+const List = () => {
+    return (
+      <div>
+        <header>
+            <h2>Things to do<span>soon</span></h2>
+            <p>Interesting things I want to check out</p>
+        </header>
+        <section>
+          <article>
+            <h1>Books</h1>
+          </article>
+          <article>
+            <h1>Movies</h1>
+          </article>
+          <article>
+            <h1>Games</h1>
+          </article>
+        </section>
+      </div>
+    );
+  };
+
+  export default List;
